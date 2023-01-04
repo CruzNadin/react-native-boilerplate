4b43044a9cf9df42d4c65c1ae958817b35b1759c
Kadir Barcın <cruznadin01@gmail.com>
2023-01-04 22:16:50 +0300

    edited

--- ios/photo_editorTests/photo_editorTests.m
+++ ios/photo_editorTests/photo_editorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface photo_editorTests : XCTestCase
+@interface TsBoilerPlateTests : XCTestCase
 
 @end
 
-@implementation photo_editorTests
+@implementation TsBoilerPlateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
