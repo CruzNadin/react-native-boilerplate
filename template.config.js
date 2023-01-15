b026b86745c3506a49f6a3c9875b2d2988820007
Kadir Barcın <47914558+CruzNadin@users.noreply.github.com>
2023-01-15 16:37:45 +0300

    Update template.config.js

--- template.config.js
+++ template.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
   // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
-  titlePlaceholder: "CruzNadir React Native Boilerplate",
+  titlePlaceholder: "CruzNadin React Native Boilerplate",
 
   // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
   templateDir: "./template",
